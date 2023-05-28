deca9345b3c9088fe032475e96fd5a88f7ec1ee7
ilyazastrognov <ilya.zastrognov@gmail.com>
2023-05-28 15:51:43 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Илья Застрожнов on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
